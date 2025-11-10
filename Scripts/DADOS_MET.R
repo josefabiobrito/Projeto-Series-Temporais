dce2428783a3d7c0b113ccaffca3afa217b5d2f6
Jose-Brito1 <jose.brito1@usp.br>
2025-11-10 17:53:50 -0300

    Acerto do código

--- Scripts/DADOS_MET.R
+++ Scripts/DADOS_MET.R
@@ -12,48 +12,8 @@ library(dplyr)
 library(lubridate)
 library(tsibble)
 
-DADOS_2025<-read.csv2("C:/Users/josef/Downloads/dados_met_2025.csv", encoding = 'latin1')
-DADOS_2024<-read.csv2("C:/Users/josef/Downloads/dados_met_2024.csv", encoding = 'latin1')
-DADOS_2023<-read.csv2("C:/Users/josef/Downloads/dados_met_2023.csv", encoding = 'latin1')
-DADOS_2022<-read.csv2("C:/Users/josef/Downloads/dados_met_2022.csv", encoding = 'latin1')
-DADOS_2021<-read.csv2("C:/Users/josef/Downloads/dados_met_2021.csv", encoding = 'latin1')
-DADOS_2020<-read.csv2("C:/Users/josef/Downloads/dados_met_2020.csv", encoding = 'latin1')
-DADOS_2019<-read.csv2("C:/Users/josef/Downloads/dados_met_2019.csv", encoding = 'latin1')
-DADOS_2018<-read.csv2("C:/Users/josef/Downloads/dados_met_2018.csv", encoding = 'latin1')
-DADOS_2017<-read.csv2("C:/Users/josef/Downloads/dados_met_2017.csv", encoding = 'latin1')
-DADOS_2016<-read.csv2("C:/Users/josef/Downloads/dados_met_2016.csv", encoding = 'latin1')
-DADOS_2015<-read.csv2("C:/Users/josef/Downloads/dados_met_2015.csv", encoding = 'latin1')
-
-DADOS_2O25<-as.Date(DADOS_2025$Data)
-DADOS_2O24<-as.Date(DADOS_2024$Data)
-DADOS_2O23<-as.Date(DADOS_2023$Data)
-DADOS_2O22<-as.Date(DADOS_2022$Data)
-DADOS_2O21<-as.Date(DADOS_2021$Data)
-DADOS_2O20<-as.Date(DADOS_2020$Data)
-DADOS_2O19<-as.Date(DADOS_2019$Data)
-DADOS_2O18<-as.Date(DADOS_2018$Data)
-DADOS_2O17<-as.Date(DADOS_2017$Data)
-DADOS_2O16<-as.Date(DADOS_2016$Data)
-DADOS_2O15<-as.Date(DADOS_2015$Data)
-
-
-DADOS_GERAL<-rbind(DADOS_2015,DADOS_2016,DADOS_2017, DADOS_2018, DADOS_2019,DADOS_2020,DADOS_2021,DADOS_2022,DADOS_2023,DADOS_2024,DADOS_2025)
-DADOS_GERAL <- DADOS_GERAL %>%
-  mutate(across(where(is.numeric), ~replace_na(., 0)))
-
-DADOS_GERAL<-DADOS_GERAL%>%
-  mutate(Data = dmy(Data))%>%
-  group_by(Data)%>%
-  summarise(
-    Temperatura_Media = mean(Temp..Ins...C., na.rm = TRUE),
-    Amplitude_Media = mean((Temp..Max...C.-Temp..Min...C.),na.rm = TRUE),
-    Umidade_Media = mean(Umi..Ins....., na.rm = TRUE),
-    Vol_Chuva = sum(Chuva..mm., na.rm = TRUE)
-  )
-head(DADOS_GERAL, n=10)
-write.csv2(DADOS_GERAL, "C:/Users/josef/OneDrive/Documentos/PUB/Projeto-Series-Temporais/Datasets/Dados_Met_SC.csv", row.names = FALSE)
-
-
+DADOS_GERAL<-read.csv2("C:/Users/josef/OneDrive/Documentos/PUB/Projeto-Series-Temporais/Datasets/Dados_met_SC.csv")
+head(DADOS_GERAL,n=10)
 
 DADOS_RESUM<-DADOS_GERAL%>%
   mutate(ano_mes = floor_date(Data, "month"))%>%
