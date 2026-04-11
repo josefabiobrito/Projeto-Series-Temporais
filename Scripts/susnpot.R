70a32a73517dc476d1174bd45114a886c29a9f0a
josefabiobrito <josefabiobrito13@gmail.com>
2026-04-10 22:37:28 -0300

    Resíduos da apostila

--- Scripts/susnpot.R
+++ Scripts/susnpot.R
@@ -8,6 +8,7 @@ library('forecast')
 library('ggplot2')
 library(openxlsx)
 library(stringr)
+library(tseries)
 
 SPY<-sunspot.year
 
@@ -28,11 +29,26 @@ show(plot)
 #       height = 6,
 #       units = "in")
 
-acf_SPY<-Acf(diff(sunspot.year), lag.max = 20, type = 'partial')
+adf.test(diff(sunspot.year))
+
+n_diffs <- 0
+
+acf_SPY <- ggAcf(sunspot.year, lag.max = 26) +
+  labs(
+    title = "Correlograma"
+  )
+
+acf_SPY
+
+pacf_SPY<-ggAcf(sunspot.year,lag.max = 26, type = 'partial')+
+  labs(
+    title = "Correlograma parcial"
+  )
+pacf_SPY
 
 #simulando AR(p):
 
-sim <- arima.sim(model = list(ar = 0.7), n = 100)
+sim <- arima.sim(model = list(ar = 0.7), n = 150)
 plot<-autoplot(sim,
                xlab = 'Tempo',
                ylab = 'Número médio',
@@ -40,83 +56,70 @@ plot<-autoplot(sim,
                color = 'blue')+
   labs(title = 'Série simulada')+
   theme_minimal()+
-  show(plot)
+show(plot)
 #Teste de modelo
+p <- as.integer(length(sunspot.year) * 0.90)
 
-treino <- head(SPY, -22)
-teste  <- tail(SPY, 22)
-fit_modelo<-Arima(treino,order = c(6,1,6))
-autoplot(forecast(fit_modelo, h=22)) +
-  autolayer(teste, series="Dados Reais") +
-  labs(title = "Previsão Manchas solares vs Realidade",
-       subtitle = str_glue("Modelo: {forecast:::arima.string(fit_modelo)}, AIC: {round(fit_modelo$aic,2)}"))+
+treino <- head(sunspot.year, p)
+teste  <- tail(sunspot.year, length(sunspot.year) - p)
+fit_806<-Arima(sunspot.year, order = c(8,0,6))
+rmse_806<-accuracy(fit_806)[1,2]
+fit_809<-Arima(sunspot.year, order = c(8,0,9))
+rmse_809<-accuracy(fit_809)[1,2]
+autoplot(SPY, ylab = "Manchas Solares")+
+  autolayer(fit_806$fitted, PI=FALSE, series=forecast:::arima.string(fit_806))+
+  autolayer(fit_809$fitted, PI=FALSE, series=forecast:::arima.string(fit_809))+
+  labs(title = "Ajuste do modelo de Manchas solares vs Realidade",
+       caption = str_glue("RMSE, AIC:\n ARIMA(8,0,9):{round(rmse_809,3)}, {round(fit_806$aic,3)} 
+                          \n ARIMA(8,0,6): {round(rmse_806,3)}, {round(fit_809$aic,3)}"))+
   theme_minimal()
 
+
+
 for (p in 1:15){
-  fit_modelo<-Arima(SPY, order = c(p,1,0))
+  fit_modelo<-Arima(sunspot.year, order = c(8,0,p))
   print(forecast:::arima.string(fit_modelo))
   print(fit_modelo$aic)
 }
 
 
+#RESÍDUOS MODELO
+
+res<-fit_809$residuals
+autoplot(res,
+         xlab = 'Tempo',
+         ylab = 'Resíduos',
+         size =0.9,
+         color = 'blue')+
+  labs(title = 'Série dos resíduos')+
+  theme_minimal()
+ 
+ 
+acf_Res <- ggAcf(res, lag.max = 26) +
+  labs(
+    title = "Correlograma"
+  )
+
+pacf_Res<-ggAcf(res,lag.max = 26, type = 'partial')+
+  labs(
+    title = "Correlograma parcial"
+  )
+
+acf_Res
+pacf_Res
+
+library(car)
+
+qqPlot(as.numeric(residuals(fit_809)), 
+       main = "Normal Q-Q Plot: Resíduos do ARIMA(8,0,9)",
+       ylab = "Quantis da Amostra",
+       xlab = "Quantis Teóricos",
+       pch = 20, 
+       col.lines = "red") 
+
 library(forecast)
 
-otimizar_arima <- function(dados, max_p=11, max_d=2, max_q=7) {
-  
-  # 1. Criar todas as combinações possíveis (O espaço de busca Z^3)
-  grid <- expand.grid(p = 5:max_p, 
-                      d = 0:max_d, 
-                      q = 0:max_q)
-  
-  # Coluna para armazenar o AIC (iniciamos com NA)
-  grid$AIC <- NA
-  
-  # Barra de progresso (opcional, mas útil para grids grandes)
-  total <- nrow(grid)
-  pb <- txtProgressBar(min = 0, max = total, style = 3)
-  
-  # 2. Loop através do Grid
-  for(i in 1:total) {
-    
-    # Extrair parâmetros da linha atual
-    p_try <- grid$p[i]
-    d_try <- grid$d[i]
-    q_try <- grid$q[i]
-    
-    # 3. Bloco de Tentativa (TryCatch)
-    # É CRUCIAL em loops ARIMA, pois alguns modelos falham ao convergir
-    resultado <- tryCatch({
-      
-      # Ajusta o modelo
-      modelo <- Arima(dados, order = c(p_try, d_try, q_try), method="ML")
-      
-      # Retorna o AIC
-      modelo$aic
-      
-    }, error = function(e) {
-      # Se der erro, retorna Infinito para que esse modelo nunca seja escolhido
-      return(Inf)
-    })
-    
-    grid$AIC[i] <- resultado
-    setTxtProgressBar(pb, i)
-  }
-  
-  close(pb)
-  
-  # 4. Limpeza e Ordenação
-  # Remove modelos que deram erro (AIC = Inf)
-  grid_limpo <- grid[grid$AIC != Inf, ]
-  
-  # Ordena do menor AIC para o maior (Minimização)
-  grid_final <- grid_limpo[order(grid_limpo$AIC), ]
-  
-  return(grid_final)
-}
+# Roda o Ljung-Box e plota os gráficos de diagnóstico
+checkresiduals(fit_809, lag = 25)
 
-# --- Exemplo de Uso com seus dados ---
-# Supondo que 'minha_serie' seja o seu objeto ts (ex: sunspot.year)
-resultado <- otimizar_arima(treino)
-
-# Visualizar o Top 5 modelos
-head(resultado, 5)
+Box.test(res, type = "Ljung-Box", lag = 25, fitdf = 17)
